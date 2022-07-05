61fa6899c5f2cec3d93635b05390426f0222de9b
aldinoyogi <aldino.yogi@gmail.com>
2022-07-05 21:36:22 +0700

    Add Changes

--- app/src/main/java/com/rana/reliefborobudur/MainActivity.java
+++ app/src/main/java/com/rana/reliefborobudur/MainActivity.java
@@ -17,6 +17,7 @@ import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
@@ -30,11 +31,13 @@ import org.opencv.ml.Ml;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.imgproc.Imgproc;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
@@ -46,10 +49,13 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -74,13 +80,19 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private List<String> listStringLabelImage = new ArrayList<String>();
     private List<Mat> listMatDatasets = new ArrayList<Mat>();
 
+    private KNearest KNN;
     private int intCameraHeight;
     private int intCameraWidth;
     private int intCropPoint = 0;
     private double doubleImageCrop = 0.2;
     private double doubleImageMaxCrop = 0.4;
     private Boolean booleanPortraitMode = false;
+    private Boolean booleanAutoCrop = false;
+    private Boolean booleanInvert = false;
+    private Boolean booleanCaptureImage = false;
     private Size sizeKnnPredict = new Size(32, 32);
+    private List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+    private Mat matHierarchy;
 
     private InputStream inputStream = null;
     private CameraBridgeViewBase mOpenCvCameraView;
@@ -93,11 +105,24 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private FrameLayout layoutView;
 
     private TextView mTextView;
+    private TextView mAutoCrop;
+    private TextView mDistance;
+
     private Button mBackButton;
     private ImageView mImageView;
     private SeekBar seekBarCrop;
+    private Switch mSwitchInvert;
 
     private Bitmap bitmapImageView;
+
+    /*========================For AutoCrop=====================*/
+    private Mat matTempImage2;
+    private MatOfPoint2f approxCurve;
+    private MatOfPoint2f contour2f;
+    private MatOfPoint points;
+    private double doubleDistance;
+    private Rect rect;
+    /*=========================================================*/
 
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -114,6 +139,8 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                         matDatasetImage = new Mat();
                         matImagePredicted = new Mat();
                         matSelectedDistance = new Mat();
+                        matHierarchy = new Mat();
+                        contours = new ArrayList<MatOfPoint>();
 
                         AssetManager assetManager = getAssets();
                         String[] stringDirPaths = assetManager.list("datasets");
@@ -160,6 +187,10 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                         }
                         matLabelIntImage.convertTo(matLabelIntImage, CvType.CV_32F);
                         mOpenCvCameraView.enableView();
+                        booleanCaptureImage = true;
+
+                        KNN = KNearest.create();
+                        KNN.train(matDatasetImage, Ml.ROW_SAMPLE, matLabelIntImage);
 
                     } catch (Exception ex)
                     {
@@ -203,7 +234,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     public void onCameraViewStarted(int width, int height) {
         intCameraHeight = height;
         intCameraWidth = width;
-        Log.i(LOG_OpenCV, String.format("Height: %d, Width: %d", height, width));
+        //Log.i(LOG_OpenCV, String.format("Height: %d, Width: %d", height, width));
     }
 
     @Override
@@ -214,30 +245,103 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     @Override
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
-        SystemClock.sleep(10);
         matCameraImage = inputFrame.rgba();
         intCropPoint = (int)(intCameraHeight*doubleImageCrop);
 
-        if (booleanPortraitMode)
+        if (booleanAutoCrop)
         {
-            Imgproc.rectangle(matCameraImage, new Point(0, intCropPoint),
-                    new Point(intCameraWidth, intCameraHeight - intCropPoint),
-                    new Scalar(255,223,0), 2);
-            Rect roi = new Rect(0, intCropPoint, intCameraWidth,
-                    intCameraHeight - (intCropPoint*2));
-            matCroppedImage = new Mat(matCameraImage, roi);
+            try { findContoursOfImage(); }
+            catch (Exception exc) {};
         }
         else
         {
-            Imgproc.rectangle(matCameraImage, new Point(intCropPoint, 0),
-                    new Point(intCameraWidth - intCropPoint, intCameraHeight),
-                    new Scalar(255,223,0), 2);
-            Rect roi = new Rect(intCropPoint, 0, intCameraWidth - (intCropPoint*2),
-                    intCameraHeight);
-            matCroppedImage = new Mat(matCameraImage, roi);
+            if (booleanPortraitMode)
+            {
+                Imgproc.rectangle(matCameraImage, new Point(0, intCropPoint),
+                        new Point(intCameraWidth, intCameraHeight - intCropPoint),
+                        new Scalar(255,223,0), 2);
+                Rect roi = new Rect(0, intCropPoint, intCameraWidth,
+                        intCameraHeight - (intCropPoint*2));
+                matCroppedImage = new Mat(matCameraImage, roi);
+            }
+            else
+            {
+                Imgproc.rectangle(matCameraImage, new Point(intCropPoint, 0),
+                        new Point(intCameraWidth - intCropPoint, intCameraHeight),
+                        new Scalar(255,223,0), 2);
+                Rect roi = new Rect(intCropPoint, 0, intCameraWidth - (intCropPoint*2),
+                        intCameraHeight);
+                matCroppedImage = new Mat(matCameraImage, roi);
+            }
+            Core.rotate(matCroppedImage, matCroppedImage, 0);
         }
-        Core.rotate(matCroppedImage, matCroppedImage, 0);
+
+
         return matCameraImage;
+    }
+
+
+    private void findContoursOfImage(){
+
+        matTempImage2 = matCameraImage.clone();
+        Imgproc.cvtColor(matCameraImage, matTempImage, Imgproc.COLOR_RGBA2GRAY);
+        if (booleanInvert)
+        {
+            Imgproc.threshold(matTempImage, matTempImage, 155,200, Imgproc.THRESH_BINARY);
+        }
+        else
+        {
+            Imgproc.threshold(matTempImage, matTempImage, 60,155, Imgproc.THRESH_BINARY_INV);
+        }
+        Imgproc.findContours(matTempImage, contours, matHierarchy,
+                Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+        matHierarchy.release();
+
+        for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++)
+        {
+            approxCurve = new MatOfPoint2f();
+            contour2f = new MatOfPoint2f(contours.get(contourIdx).toArray());
+
+            doubleDistance = Imgproc.arcLength(contour2f, true)*0.01;
+            Imgproc.approxPolyDP(contour2f, approxCurve, doubleDistance, true);
+
+            points = new MatOfPoint(approxCurve.toArray());
+            rect = Imgproc.boundingRect(points);
+
+            double doubleHeight = rect.height;
+            double doubleWidth = rect.width;
+
+            if (doubleWidth > Math.min(intCameraWidth, intCameraHeight)/5
+                && doubleHeight > Math.min(intCameraWidth, intCameraHeight)/5
+                && doubleHeight < intCameraHeight-20
+                && doubleWidth < intCameraWidth-20)
+            {
+                int threshold = 5;
+                double currentX = rect.x + threshold;
+                double currentY = rect.y + threshold;
+                double distanceX = doubleWidth - threshold*2;
+                double distanceY = doubleHeight - threshold*2;
+                Imgproc.rectangle(matCameraImage, new Point(currentX, currentY),
+                        new Point(currentX + distanceX, currentY + distanceY),
+                        new Scalar(255,223,0), 3);
+                try
+                {
+                    Rect roi = new Rect((int)currentX,
+                                (int)currentY,
+                                (int) distanceX,
+                                (int) distanceY);
+                    matCroppedImage = new Mat(matTempImage2, roi);
+                    Core.rotate(matCroppedImage, matCroppedImage, 0);
+                } catch (Exception exc) { Log.i(LOG_OpenCV, exc.getMessage()); }
+                matHierarchy.release();
+                contours.clear();
+                break;
+            }
+            else
+            {
+                matCroppedImage = null;
+            }
+        }
     }
 
 
@@ -252,15 +356,49 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         mOpenCvCameraView.setCvCameraViewListener(this);
 
         mTextView = (TextView) findViewById(R.id.textview_prediction);
+        mDistance = (TextView) findViewById(R.id.textview_distance);
         mImageView = (ImageView) findViewById(R.id.imageview_captured);
 
         mTextView.setTypeface(null, BOLD);
 
+        onClickAutoCropImage();
+        onChangeSwitchInvert();
         onChangeSeekbarForCropping();
         onChangeOrientationCropping();
         onClickCaptureImage();
         onClickBackViewCamera();
 
+    }
+
+
+    private void onChangeSwitchInvert(){
+        mSwitchInvert = (Switch) findViewById(R.id.switch_invert);
+        mSwitchInvert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                booleanInvert = !booleanInvert;
+            }
+        });
+    }
+
+    private void onClickAutoCropImage(){
+        mAutoCrop = (TextView) findViewById(R.id.fab_auto_crop);
+        mAutoCrop.setTextColor(Color.parseColor("#ffffff"));
+        mAutoCrop.setTypeface(null, BOLD);
+        mAutoCrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                booleanAutoCrop = !booleanAutoCrop;
+                if (booleanAutoCrop)
+                {
+                    mAutoCrop.setTextColor(Color.parseColor("#ffb600"));
+                }
+                else
+                {
+                    mAutoCrop.setTextColor(Color.parseColor("#ffffff"));
+                }
+            }
+        });
     }
 
     private void onChangeSeekbarForCropping(){
@@ -318,10 +456,15 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (matCroppedImage != null){
+                if (matCroppedImage != null && booleanCaptureImage){
                     layoutMain.setVisibility(View.GONE);
                     layoutView.setVisibility(View.VISIBLE);
-                    predictCurrentImage();
+                    try
+                    {
+                        SystemClock.sleep(300);
+                        predictCurrentImage();
+                    }
+                    catch (Exception exc) { }
                 }
             }
         });
@@ -358,35 +501,36 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 
                 for (int colPix = 0; colPix < pixelSelectedImage.length; colPix++)
                 {
-                    totalEuc = totalEuc + (Math.pow(pixelTempImage[colPix], 2)
-                            - Math.pow(pixelSelectedImage[colPix], 2));
+                    if (colPix != 3)
+                    {
+                        totalEuc = totalEuc + Math.pow(pixelTempImage[colPix]
+                                - pixelSelectedImage[colPix], 2);
+                    }
                 }
             }
         }
 
         totalEuc = Math.sqrt(Math.abs(totalEuc));
-        Log.i(LOG_OpenCV, String.format("Distance: %f", totalEuc));
+        //Log.i(LOG_OpenCV, String.format("Distance: %f", totalEuc));
         matSelectedDistance.release();
 
         return totalEuc;
     }
 
+
     private void predictCurrentImage(){
 
-        showImagePreview();
         Imgproc.resize(matCroppedImage, matTempImage, sizeKnnPredict);
 
         matTempImage = matTempImage.reshape(1,1);
         matTempImage.convertTo(matTempImage, CvType.CV_32F);
 
-        KNearest KNN = KNearest.create();
-        KNN.train(matDatasetImage, Ml.ROW_SAMPLE, matLabelIntImage);
-
         floatNearestValue = KNN.findNearest(matTempImage,1,matImagePredicted);
         stringLabelPrediction = listStringLabelImage.get((int)floatNearestValue);
+        showImagePreview();
 
         double doubleDistance = findDistance((int)floatNearestValue);
-        if (doubleDistance > 6500 || doubleDistance < 100)
+        if (doubleDistance < 2800)
         {
             mTextView.setText(stringLabelPrediction);
         }
@@ -394,6 +538,8 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         {
             mTextView.setText("Unknown");
         }
+        mDistance.setText(String.format("Distance: %.2f", doubleDistance));
+        mDistance.setTypeface(null, Typeface.ITALIC);
     }
 
 
